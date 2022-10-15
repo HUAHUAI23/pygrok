d00ab30e4cd8b6a62169bfc40c65a5079a154e42
i42 <i42@hi>
2022-10-15 09:49:20 +0800

    add gorkkk demo and update .gitignore (add my python venv environment)

---
+++ gorkkk.py
@@ -0,0 +1,28 @@
+import regex as re
+# NOTE re can't handle redefinition of group name
+
+types = {
+    'WORD': r'\w+',
+    'NUMBER': r'\d+',
+    # todo: extend me
+}
+
+
+def compile(pat):
+    return re.sub(
+        r'%{(\w+):(\w+)}',
+        lambda m: "(?P<" + m.group(2) + ">" + types[m.group(1)] + ")", pat)
+
+
+patterCompiled = compile("%{WORD:method}-%{NUMBER:bytes}-%{NUMBER:duration}")
+# patterCompiled: (?P<method>\w+)-(?P<bytes>\d+)-(?P<duration>\d+)
+print(patterCompiled)
+# groups group groupdict
+print(re.search(patterCompiled, "hello-123-456").groupdict())
+# {'duration': '456', 'bytes': '123', 'method': 'hello'}
+
+# TODO Dissection
+# Dissection
+# pat = r"\b([^\W\d_])([^\W\d_]*)([^\W\d_])\b"
+# s = "Testers"
+# print(re.sub(pat, (lambda m: "{0}{1}{2}".format(m.group(1), len(''.join(set(m.group(2)))), m.group(3))), s))
---
+++ mytest/test.py
@@ -0,0 +1,6 @@
+from pygrok import Grok
+
+text = 'gary is male, 25 years old and weighs 68.5 kilograms'
+pattern = '%{WORD:name} is %{WORD:gender}, %{NUMBER:age:int} years old and weighs %{NUMBER:weight:float} kilograms'
+grok = Grok(pattern)
+print(grok.match(text))
